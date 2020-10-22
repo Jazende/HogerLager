305a8c5396839d678890c309bb5b9e920c0f46b3
Karel De Ridder <Karel.DeRidder@rodekruis.be>
2020-10-22 11:09:07 +0200

    move button functionality to seperate class

--- hogerlager.py
+++ hogerlager.py
@@ -4,38 +4,64 @@ import random
 from pyglet.gl import *
 
 font_data = {'font_name': 'Times New Roman', 'font_size': 32, 'color': (0, 0, 0, 255), 'anchor_x': 'center', 'anchor_y': 'center'}
+suits = ("Clubs", "Diamonds", "Hearths", "Spades")
 
 def get_scaling(image_width, image_height, width, height):
-    if width is None:
-        scale_y = height / image_height
-        scale_x = scale_y
-    elif height is None:
-        scale_x = width / image_width
-        scale_y = scale_x
-    else:
-        scale_y = height / image_height
-        scale_x = width / image_width
+    ''' Set scaling of X/Y as proportion of measurements if avail, if not use scaling of other measurements. '''
+    scale_x = width / image_width if not width is None else height / image_height
+    scale_y = height / image_height if not height is None else width / image_width 
     return scale_x, scale_y
 
 def higher(x, y):
-    if not isinstance(x, Card):
-        raise TypeError
-    if not isinstance(y, Card):
+    if not (isinstance(x, Card) and isinstance(y, Card)):
         raise TypeError
     return x.number > y.number
 
 def lower(x, y):
-    if not isinstance(x, Card):
-        raise TypeError
-    if not isinstance(y, Card):
+    if not (isinstance(x, Card) and isinstance(y, Card)):
         raise TypeError
     return x.number < y.number
 
+def relative_path(*args):
+    return os.path.join(os.getcwd(), *args)
+
 def get_card_image_path(suit_nr, card_nr):
-    return os.path.join(os.getcwd(), 'resources', f'cards_{suit_nr+1}x{card_nr+1}.png')
+    return relative_path('resources', f'cards_{suit_nr+1}x{card_nr+1}.png')
 
 def sprite_from_path(path):
     return pyglet.sprite.Sprite(pyglet.image.load(path))
+
+class Button(pyglet.sprite.Sprite):
+    def __init__(self, path, root, connect):
+        super().__init__(pyglet.image.load(relative_path(*path)))
+        self.root = root
+        self.connected_function = None
+        if connect:
+            self.connect(connect)
+
+    @property
+    def left(self):
+        return self.x
+
+    @property
+    def right(self):
+        return self.x + self.width
+
+    @property
+    def bottom(self):
+        return self.y
+
+    @property
+    def top(self):
+        return self.y + self.height
+    
+    def connect(self, function):
+        self.connected_function = function
+        self.root.push_handlers(self.on_mouse_press)
+
+    def on_mouse_press(self, mouse_x, mouse_y, button, modifiers):
+        if self.left <= mouse_x <= self.right and self.bottom <= mouse_y <= self.top:
+            self.connected_function()
 
 class Card:
     def __init__(self, suit, suit_nr, number):
@@ -45,15 +71,22 @@ class Card:
 
 class Deck:
     def __init__(self):
-        self._cards = [Card(suit, suit_nr, card_nr) for card_nr in range(13) for suit_nr, suit in enumerate(["Clubs", "Diamonds", "Hearths", "Spades"])]
+        self._cards = [Card(suit, suit_nr, card_nr) for card_nr in range(13) for suit_nr, suit in enumerate(suits)]
 
     def shuffle(self):
         random.shuffle(self._cards)
+    
+    def update_sprites(self, **kwargs):
+        for card in self._cards:
+            card.image.update(**kwargs)
 
 class HogerLager(Deck):
-    def __init__(self, x, y, width=None, height=None):
+    def __init__(self, root, x, y, width=None, height=None):
         super().__init__()
+        self.root = root
+        self.root.push_handlers(self.on_mouse_press)
         self.finished = False
+        self.show_score = False
         self.x = x
         self.y = y
 
@@ -63,29 +96,32 @@ class HogerLager(Deck):
         scaled_image_width = base_image_width*scale_x
         scaled_image_height = base_image_height*scale_y
 
-        self.up = sprite_from_path(os.path.join(os.getcwd(), 'resources', 'up.png'))
-        self.down = sprite_from_path(os.path.join(os.getcwd(), 'resources', 'down.png'))
+        self.up = Button(root=self.root, path=('resources', 'up.png'), connect=self.press_up)
+        self.down = Button(root=self.root, path=('resources', 'down.png'), connect=self.press_down)
         self.up.update(x=x, y=y-self.up.image.height*2-10, scale_x=scale_x*2, scale_y=scale_y*2)
         self.down.update(x=x+scaled_image_width-self.down.image.width*2-10, y=y-self.down.image.height*2-10, scale_x=scale_x*2, scale_y=scale_y*2)
 
-        for card in self._cards:
-            card.image.update(x=x, y=y, scale_x=scale_x, scale_y=scale_y)
+        self.update_sprites(x=x, y=y, scale_x=scale_x, scale_y=scale_y)
 
         self.score_label = pyglet.text.Label('0', x=self.x+scaled_image_width//2, y=self.y-self.up.height//2, **font_data)
         self.finished_label = pyglet.text.Label(' ', x=self.x+scaled_image_width//2, y=self.y-3*self.up.height//2, **font_data)
 
         self.start()
 
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def score(self, value):
+        self._score = value
+        self.score_label.text = f'{self.score}'
+
     def start(self):
         self.shuffle()
         self.index = 0
         self.score = 0
         self.finished = False
-        self.update_score()
-
-    def update_score(self):
-        self.score_label.text = f'{self.score}'
-        self.finished_label.text = f'Je hebt {self.score} punten. Klik om te herstarten.'
 
     def draw(self):
         if not self.finished:
@@ -95,17 +131,12 @@ class HogerLager(Deck):
             self.score_label.draw()
         else:
             self.finished_label.draw()
-    
+
     def on_mouse_press(self, mouse_x, mouse_y, button, modifiers):
-        if self.finished: 
+        if self.finished and self.show_score: 
+            self.show_score = False
+        elif self.finished and not self.show_score:
             self.start()
-        else:
-            if self.up.x <= mouse_x  <= self.up.x + self.up.width and \
-                self.up.y <= mouse_y <= self.up.y + self.up.height:
-                self.press_up()
-            elif self.down.x <= mouse_x  <= self.down.x + self.down.width and \
-                self.down.y <= mouse_y <= self.down.y + self.down.height:
-                self.press_down()
     
     def press_up(self):
         self.next_card(lower)
@@ -120,18 +151,17 @@ class HogerLager(Deck):
 
         if comparison(cur_value, next_value):
             self.score += 1
-            self.update_score()
 
         if self.index == len(self._cards)-1:
+            self.finished_label.text = f'Je hebt {self.score} punten. Klik om te herstarten.'
             self.finished = True
+            self.show_score = True
 
 class Screen(pyglet.window.Window):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._events_passthrough = {'on_mouse_press': []}
         glClearColor(1, 1, 1, 1)
-        self.deck = HogerLager(self.width//5*2, self.height//2, self.width//4)
-        self._events_passthrough['on_mouse_press'].append(self.deck)
+        self.deck = HogerLager(self, self.width//5*2, self.height//2, self.width//4)
 
     def on_draw(self):
         self.clear()
@@ -147,11 +177,6 @@ class Screen(pyglet.window.Window):
         glOrtho(0, max(1, width), 0, max(1, height), -1, 1)
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
-
-    def on_mouse_press(self, mouse_x, mouse_y, button, modifiers):
-        if 'on_mouse_press' in self._events_passthrough:
-            for passthrough in self._events_passthrough['on_mouse_press']:
-                passthrough.on_mouse_press(mouse_x, mouse_y, button, modifiers)
 
 def main():
     screen = Screen(width=800, height=640)
